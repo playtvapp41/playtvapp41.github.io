280107925ecc80d70ed125e4e26b7debda72a184
playtvapp41.github.io <dexchainxmd@gmail.com>
2026-06-22 10:40:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './playtvapp41/',
+    base: './playtvapp41.github.io/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
